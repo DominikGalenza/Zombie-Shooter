199c498dbbb19a888937578c1161f262eca63540
Dominik Gałenza <dominik.galenza@gmail.com>
2022-01-01 20:49:38 +0100

    Added weapon zoom

---
+++ Assets/Player/WeaponZoom.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera playerCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedOutSensitivityX = 2f;
+    [SerializeField] float zoomedOutSensitivityY = 2f;
+    [SerializeField] float zoomedInSensitivityX = 1f;
+    [SerializeField] float zoomedInSensitivityY = 1f;
+
+    RigidbodyFirstPersonController playerController;
+
+    bool zoomedIn;
+
+    void Start()
+    {
+        playerController = GetComponent<RigidbodyFirstPersonController>();
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            if(zoomedIn == false)
+            {
+                zoomedIn = true;
+                playerCamera.fieldOfView = zoomedInFOV;
+                playerController.mouseLook.XSensitivity = zoomedInSensitivityX;
+                playerController.mouseLook.YSensitivity = zoomedInSensitivityY;
+                
+            }
+            else
+            {
+                zoomedIn = false;
+                playerCamera.fieldOfView = zoomedOutFOV;
+                playerController.mouseLook.XSensitivity = zoomedOutSensitivityX;
+                playerController.mouseLook.YSensitivity = zoomedOutSensitivityY;
+            }
+        }
+    }
+}
